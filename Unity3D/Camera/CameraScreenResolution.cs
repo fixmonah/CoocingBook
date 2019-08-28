b080ead2eddc42da1def210eef75b866a3c5c2de
Максим Литвиненко <fixmonah@gmail.com>
2019-08-28 17:33:46 +0300

    Scale 2D camera

---
+++ Unity3D/Camera/CameraScreenResolution.cs
@@ -0,0 +1,39 @@
+﻿// Thanks Youtube 
+// Adjust Camera to Multiple Resolutions - Unity
+// https://www.youtube.com/watch?v=TYNF5PifSmA
+
+using UnityEngine;
+using System.Collections;
+public class CameraScreenResolution : MonoBehaviour
+{
+    public bool maintainWidth = true;
+    [Range (-1,1)]
+    public int adaptPosition;
+    float defaultWidth;
+    float defaultHeight;
+    Vector3 CameraPos;
+
+
+    void Start() {
+        CameraPos = Camera.main.transform.position;
+        defaultHeight = Camera.main.orthographicSize;
+        defaultWidth = Camera.main.orthographicSize * Camera.main.aspect;
+    }
+
+    void Update()
+    {
+        if (maintainWidth)
+        {
+            Camera.main.orthographicSize = defaultWidth / Camera.main.aspect;
+
+            Camera.main.transform.position = new Vector3(CameraPos.x, adaptPosition * (defaultHeight - Camera.main.orthographicSize), CameraPos.z);
+
+        }
+        else
+        {
+            Camera.main.transform.position = new Vector3(adaptPosition * (defaultWidth - Camera.main.orthographicSize * Camera.main.aspect), CameraPos.y, CameraPos.z);
+            
+        }
+
+    }
+}
